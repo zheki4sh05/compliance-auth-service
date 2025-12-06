de8d145c70a1ec1405ca2c939567f466373706c0
evgluzedev <evgeniy.shostak.04@mail.ru>
2025-12-06 21:24:43 +0300

    init project

---
+++ src/main/java/com/trustflow/compliance_auth_service/ComplianceAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trustflow.compliance_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplianceAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplianceAuthServiceApplication.class, args);
+	}
+
+}
